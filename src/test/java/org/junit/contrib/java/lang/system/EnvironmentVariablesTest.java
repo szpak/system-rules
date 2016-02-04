ca2ee94551a7b8cb0917f2bb056aacfa834b1f19
Stefan Birkner <mail@stefan-birkner.de>
2016-02-04 23:12:17 +0100

    test flag

--- src/test/java/org/junit/contrib/java/lang/system/EnvironmentVariablesTest.java
+++ src/test/java/org/junit/contrib/java/lang/system/EnvironmentVariablesTest.java
@@ -4,6 +4,7 @@ package org.junit.contrib.java.lang.system;
 import org.junit.Test;
 import org.junit.runners.model.Statement;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,8 +12,12 @@ import static java.lang.System.getenv;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.contrib.java.lang.system.Executor.executeTestThatThrowsExceptionWithRule;
 import static org.junit.contrib.java.lang.system.Executor.executeTestWithRule;
+import static org.junit.contrib.java.lang.system.Statements.SUCCESSFUL_TEST;
 
 public class EnvironmentVariablesTest {
+	private static final boolean ACCESSIBLE_FLAG_OF_ENVIRONMENT_VARIABLES_MAP_BEFORE_FIRST_INTERACTION_WITH_RULE
+		= isMapOfEnvironmentVariablesAccessible();
+
 	@Test
 	public void after_a_successful_test_environment_variables_map_contains_same_values_as_before() {
 		Map<String, String> originalEnvironmentVariables = new HashMap<String, String>(getenv());
@@ -113,5 +118,35 @@ public class EnvironmentVariablesTest {
 				assertThat(getenv()).doesNotContainKey("dummy name");
 			}
 		}, environmentVariables);
+	}
+
+	@Test
+	public void after_setting_an_environment_variable_the_accessible_flag_of_the_field_for_map_of_environment_variables_is_the_same_as_before() {
+		final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+		executeTestWithRule(new Statement() {
+			@Override
+			public void evaluate() {
+				environmentVariables.set("dummy name", "dummy value");
+				assertThat(isMapOfEnvironmentVariablesAccessible()).isEqualTo(
+					ACCESSIBLE_FLAG_OF_ENVIRONMENT_VARIABLES_MAP_BEFORE_FIRST_INTERACTION_WITH_RULE);
+			}
+		}, environmentVariables);
+	}
+
+	@Test
+	public void after_a_test_the_field_for_map_of_environment_variables_has_unchanged_accessible_flag() {
+		EnvironmentVariables environmentVariables = new EnvironmentVariables();
+		executeTestWithRule(SUCCESSFUL_TEST, environmentVariables);
+		assertThat(isMapOfEnvironmentVariablesAccessible()).isEqualTo(
+			ACCESSIBLE_FLAG_OF_ENVIRONMENT_VARIABLES_MAP_BEFORE_FIRST_INTERACTION_WITH_RULE);
+	}
+
+	private static boolean isMapOfEnvironmentVariablesAccessible() {
+		try {
+			Field field = getenv().getClass().getDeclaredField("m");
+			return field.isAccessible();
+		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
